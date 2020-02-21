4eb6056c1c967061b18a9feaa16f9b221e0274af
Ali Kamran <43109679+alikamranch@users.noreply.github.com>
2020-02-21 05:05:05 +0500

    Navbar Created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 import Home from '../Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse,
+    MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBContainer, MDBBtn
+} from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
+import './Navbar.css';
+import logo from "../../assets/images/WERide Transparent.png";
+
+
+class Navbar extends Component {
+    state = {
+        isOpen: false
+    };
+
+    toggleCollapse = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+        return (
+            <Router>
+                <MDBNavbar color="#fafafa grey lighten-5" light expand="md">
+                    <MDBNavbarBrand>
+                        <img
+                            src={logo}
+                            width="210"
+                            height="70"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </MDBNavbarBrand>
+                    <MDBContainer>
+                        <MDBNavbarToggler onClick={this.toggleCollapse} />
+                        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                            <MDBNavbarNav left>
+                                <MDBNavItem active>
+                                    <MDBNavLink to="#!">Home</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#!">About</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#!">How it works</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBDropdown>
+                                        <MDBDropdownToggle nav caret>
+                                            <span className="mr-2">Dropdown</span>
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu>
+                                            <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+                                            <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+                                            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                                            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                            <MDBNavbarNav right>
+                                <MDBNavItem>
+                                    <MDBBtn color="#06c432">Sign In</MDBBtn>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                    </MDBContainer>
+
+                </MDBNavbar>
+            </Router>
+        );
+    }
+}
+
+export default Navbar;
