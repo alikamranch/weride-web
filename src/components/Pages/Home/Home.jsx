9e18089ab0ab8b9dbdb91eb4d2c69e38fe5fdbd8
Ali Kamran <43109679+alikamranch@users.noreply.github.com>
2020-02-29 04:06:35 +0500

    Home page with all of the media

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdbreact';
-import './Home.css'
+import './Home.css';
 import carpool from '../../../assets/images/car-sharing.png';
 import carpool2 from '../../../assets/images/car-sharing 2.png';
+import carpool3 from '../../../assets/images/car-sharing 3.png';
+import driverCarpool from '../../../assets/images/driver-carpool.png';
+import driverCarpool2 from '../../../assets/images/driver-carpool 2.png';
+import driverCarpool3 from '../../../assets/images/driver-carpool 3.png';
+import icon1 from '../../../assets/images/WeRide Icons 01.png';
+import icon2 from '../../../assets/images/WeRide Icons 02.png';
+import icon3 from '../../../assets/images/WeRide Icons 03.png';
+import icon4 from '../../../assets/images/WeRide Icons 04.png';
 
 function Home() {
     const headingStyle = {
@@ -11,7 +19,7 @@ function Home() {
         fontWeight: "450",
         fontSize: "3.5rem"
 
-    }
+    };
 
     return (
         <div>
@@ -75,7 +83,7 @@ function Home() {
                 <br />
                 <MDBRow className="justify-content-center">
                     <MDBCol lg="3">
-                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder" width="140" height="140"></img>
+                        <img src={icon1} alt="Economical icon" width="170" height="180"></img>
                         <br />
                         <br />
                         <h2 className="carpool-headings">Economical</h2>
@@ -84,7 +92,7 @@ function Home() {
                     <br />
                     <br />
                     <MDBCol lg="3">
-                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder" width="140" height="140"></img>
+                        <img src={icon2} alt="Traffic icon" width="185" height="180"></img>
                         <br />
                         <br />
                         <h2 className="carpool-headings">Less Traffic</h2>
@@ -93,7 +101,7 @@ function Home() {
                     <br />
                     <br />
                     <MDBCol lg="3">
-                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder" width="140" height="140"></img>
+                        <img src={icon3} alt="Pollution icon" width="170" height="180"></img>
                         <br />
                         <br />
                         <h2 className="carpool-headings">Reduced Pollution</h2>
@@ -102,7 +110,7 @@ function Home() {
                     <br />
                     <br />
                     <MDBCol lg="3">
-                        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder" width="140" height="140"></img>
+                        <img src={icon4} alt="Socialize icon" width="178" height="177"></img>
                         <br />
                         <br />
                         <h2 className="carpool-headings">Socialize</h2>
@@ -135,7 +143,7 @@ function Home() {
                     </MDBRow>
                 </div>
             </MDBContainer>
-            {/* Featurette end */}
+
             <br />
             <br />
             <br />
@@ -158,6 +166,95 @@ function Home() {
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>
+
+            <MDBContainer fluid style={{ padding: "50px" }}>
+                <MDBRow>
+
+                    <MDBCol lg="6" className="text-left">
+                        <br />
+                        <br />
+                        <br />
+                        <h1 className="home-featurette-heading">Travel on the same route and save both money and time.</h1>
+                        <p className="home-featurette-paragragh ">WERide works by connecting the rides of people who have the same destination and route. The fare is divided among the number of riders and you save time by riding together.</p>
+                    </MDBCol>
+
+                    <MDBCol lg="6" className="img-fluid">
+                        <img src={carpool3} alt="carpool-concept" width="700" height="700" className="mx-auto img-fluid" />
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+            {/* Featurette end */}
+
+
+
+            {/* Driver Featurette */}
+            <div>
+                <hr className="home-hr-style" />
+                <MDBContainer>
+                    <br />
+                    <br />
+                    <br />
+                    <h1 className="home-featurette-heading">Why Drive for us?</h1>
+                </MDBContainer>
+
+                <MDBContainer fluid style={{ padding: "50px" }}>
+                    <MDBRow>
+
+                        <MDBCol lg="6" className="img-fluid">
+                            <img src={driverCarpool} alt="carpool-concept" width="500" height="500" className="mx-auto img-fluid" />
+                        </MDBCol>
+
+                        <MDBCol lg="6" className="text-left">
+                            <br />
+                            <br />
+                            <br />
+                            <h1 className="home-featurette-heading">Financial Stability.</h1>
+                            <p className="home-featurette-paragragh ">The rising expense of car booking applications has led to people seeking shared rides. The high demand will result in a steady income.</p>
+                        </MDBCol>
+
+
+                    </MDBRow>
+                </MDBContainer>
+
+                <MDBContainer fluid style={{ padding: "50px" }}>
+                    <MDBRow>
+
+                        <MDBCol lg="6" className="text-left">
+                            <br />
+                            <br />
+                            <br />
+                            <h1 className="home-featurette-heading">Best Customers.</h1>
+                            <p className="home-featurette-paragragh ">It is WERide's utmost priority to bring you the best customers for a pleasant and hassle-free trip.</p>
+                        </MDBCol>
+
+                        <MDBCol lg="6" className="img-fluid">
+                            <img src={driverCarpool2} alt="carpool-concept" width="500" height="500" className="mx-auto img-fluid" />
+                        </MDBCol>
+
+
+                    </MDBRow>
+                </MDBContainer>
+
+                <MDBContainer fluid style={{ padding: "50px" }}>
+                    <MDBRow>
+
+                        <MDBCol lg="6" className="img-fluid">
+                            <img src={driverCarpool3} alt="carpool-concept" width="400" height="400" className="mx-auto img-fluid" />
+                        </MDBCol>
+
+                        <MDBCol lg="6" className="text-left">
+                            <br />
+                            <br />
+                            <h1 className="home-featurette-heading">Independance.</h1>
+                            <p className="home-featurette-paragragh ">Drivers will have the comfort of working whenever and wherever they want and at a time of their convenience.</p>
+                        </MDBCol>
+
+
+                    </MDBRow>
+                </MDBContainer>
+
+            </div>
+            {/* Driver Featurette End */}
 
 
 
