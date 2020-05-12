4f658abd4db1c187600b17acb654219f930b5959
Ali Kamran <43109679+alikamranch@users.noreply.github.com>
2020-05-12 08:26:24 +0500

    add funds feature implemented

--- src/components/Pages/RiderPages/RiderHome/RiderHome.jsx
+++ src/components/Pages/RiderPages/RiderHome/RiderHome.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
-import { MDBContainer } from "mdbreact";
+import {
+  MDBContainer,
+  MDBCarousel,
+  MDBCarouselCaption,
+  MDBCarouselInner,
+  MDBCarouselItem,
+  MDBView,
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn,
+} from "mdbreact";
 import "./RiderHome.css";
+
+import carousel1 from "../../../../assets/images/carousel/carousel1.png";
+import carousel2 from "../../../../assets/images/carousel/carousel2.jpg";
+import carousel3 from "../../../../assets/images/carousel/carousel3.jpg";
+import carousel4 from "../../../../assets/images/carousel/carousel4.jpg";
+import ten from "../../../../assets/images/Discounts/10.png";
+import fifteen from "../../../../assets/images/Discounts/15.png";
+import twenty from "../../../../assets/images/Discounts/20.png";
 
 export default class RiderHome extends Component {
   constructor(props) {
@@ -16,9 +40,226 @@ export default class RiderHome extends Component {
         <Helmet>
           <title>Home</title>
         </Helmet>
-        <MDBContainer>
-          <h1>Rider Home</h1>
-        </MDBContainer>
+        {/* Carousel Start */}
+        <div className="mt-5">
+          <MDBContainer>
+            <h1 className="text-left">Welcome</h1>
+            <hr />
+            <MDBCarousel
+              activeItem={1}
+              length={4}
+              showControls={true}
+              showIndicators={true}
+              className="z-depth-1"
+            >
+              <MDBCarouselInner>
+                <MDBCarouselItem itemId="1">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={carousel1}
+                      alt="First slide"
+                    />
+                  </MDBView>
+                  <MDBCarouselCaption>
+                    <h3 className="h3-responsive">
+                      <b>With WERide you can travel...</b>
+                    </h3>
+                    {/* <p>First text</p> */}
+                  </MDBCarouselCaption>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="2">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={carousel2}
+                      alt="Second slide"
+                    />
+                    <MDBMask overlay="black-light" />
+                  </MDBView>
+                  <MDBCarouselCaption>
+                    <h3 className="h3-responsive">
+                      <b>Smarter...</b>
+                    </h3>
+                  </MDBCarouselCaption>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="3">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={carousel3}
+                      alt="Third slide"
+                    />
+                    <MDBMask overlay="black-slight" />
+                  </MDBView>
+                  <MDBCarouselCaption>
+                    <h3 className="h3-responsive">
+                      <b>Faster...</b>
+                    </h3>
+                  </MDBCarouselCaption>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="4">
+                  <MDBView>
+                    <img
+                      className="d-block w-100"
+                      src={carousel4}
+                      alt="Fourth slide"
+                    />
+                    <MDBMask overlay="black-slight" />
+                  </MDBView>
+                  <MDBCarouselCaption>
+                    <h3 className="h3-responsive">
+                      <b>Easier...</b>
+                    </h3>
+                  </MDBCarouselCaption>
+                </MDBCarouselItem>
+              </MDBCarouselInner>
+            </MDBCarousel>
+          </MDBContainer>
+        </div>
+        {/* carousel end */}
+
+        {/* Promos start */}
+        <div className="mt-5">
+          <MDBContainer fluid>
+            <MDBContainer>
+              <h1 className="text-left">Promos</h1>
+              <hr />
+              <MDBRow>
+                <MDBCol md="4" className="mt-5">
+                  <MDBCol style={{ maxWidth: "22rem" }}>
+                    <MDBCard>
+                      <MDBCardImage className="img-fluid" src={ten} waves />
+                      <MDBCardBody>
+                        <MDBCardTitle>
+                          <b>Get 10% off!</b>
+                        </MDBCardTitle>
+                        <MDBCardText>
+                          Get 10% off on your next ride.
+                        </MDBCardText>
+                        <MDBBtn href="#" color="orange">
+                          Apply
+                        </MDBBtn>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                </MDBCol>
+
+                <MDBCol md="4" className="mt-5">
+                  <MDBCol style={{ maxWidth: "22rem" }}>
+                    <MDBCard>
+                      <MDBCardImage className="img-fluid" src={fifteen} waves />
+                      <MDBCardBody>
+                        <MDBCardTitle>
+                          <b>Get 20% off!</b>
+                        </MDBCardTitle>
+                        <MDBCardText>Get 20% off your next ride.</MDBCardText>
+                        <MDBBtn href="#" color="orange">
+                          Apply
+                        </MDBBtn>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                </MDBCol>
+
+                <MDBCol md="4" className="mt-5">
+                  <MDBCol style={{ maxWidth: "22rem" }}>
+                    <MDBCard>
+                      <MDBCardImage className="img-fluid" src={twenty} waves />
+                      <MDBCardBody>
+                        <MDBCardTitle>
+                          <b>Get 30% off!</b>
+                        </MDBCardTitle>
+                        <MDBCardText>Get 30% off your next ride.</MDBCardText>
+                        <MDBBtn href="#" color="orange">
+                          Apply
+                        </MDBBtn>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </MDBContainer>
+        </div>
+        {/* Promos end */}
+
+        {/* Latest news start */}
+        <div className="mt-5">
+          <MDBContainer fluid>
+            <MDBContainer>
+              <h1 className="text-left">Latest News</h1>
+              <hr />
+            </MDBContainer>
+
+            <MDBRow>
+              <MDBCol md="3" className="mt-5">
+                <MDBCard
+                  className="card-body"
+                  style={{ marginTop: "1rem" }}
+                  color="secondary-color-dark"
+                >
+                  <MDBCardTitle className="rider-home-text-color">
+                    <b>New fleet coming soon!</b>
+                  </MDBCardTitle>
+                  <MDBCardText className="rider-home-text-color">
+                    We wil be adding more vehicles in our fleet on launch for
+                    you to enjoy your daily commutes.
+                  </MDBCardText>
+                </MDBCard>
+              </MDBCol>
+              <MDBCol md="3" className="mt-5">
+                <MDBCard
+                  className="card-body"
+                  style={{ marginTop: "1rem" }}
+                  color="info-color-dark"
+                >
+                  <MDBCardTitle className="rider-home-text-color">
+                    <b>New Packages on their way!</b>
+                  </MDBCardTitle>
+                  <MDBCardText className="rider-home-text-color">
+                    We wil be adding more packages and we will also be adding a
+                    feature that will allow you to make your custom package.
+                  </MDBCardText>
+                </MDBCard>
+              </MDBCol>
+              <MDBCol md="3" className="mt-5">
+                <MDBCard
+                  className="card-body"
+                  style={{ marginTop: "1rem" }}
+                  color="success-color-dark"
+                >
+                  <MDBCardTitle className="rider-home-text-color">
+                    <b>Chat feature coming soon!</b>
+                  </MDBCardTitle>
+                  <MDBCardText className="rider-home-text-color">
+                    We wil be adding chat to the App so that you can communicate
+                    with your driver before confirming your ride.
+                  </MDBCardText>
+                </MDBCard>
+              </MDBCol>
+              <MDBCol md="3" className="mt-5">
+                <MDBCard
+                  className="card-body"
+                  style={{ marginTop: "1rem" }}
+                  color="warning-color-dark"
+                >
+                  <MDBCardTitle className="rider-home-text-color">
+                    <b>Debit/Credit Card payment</b>
+                  </MDBCardTitle>
+                  <MDBCardText className="rider-home-text-color">
+                    We wil be adding card payment method option so that you dont
+                    always have to pay with cash after your ride ends.
+                  </MDBCardText>
+                </MDBCard>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </div>
+        {/* Latest news end */}
+        <br />
+        <br />
+        <br />
       </div>
     );
   }
--- src/components/Pages/RiderPages/RiderProfile/RiderProfile.jsx
+++ src/components/Pages/RiderPages/RiderProfile/RiderProfile.jsx
@@ -1,27 +1,268 @@
-import React, { Component } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
-import { MDBContainer } from "mdbreact";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBTable,
+  MDBTableBody,
+  MDBBtn,
+  MDBModal,
+  MDBModalHeader,
+  MDBModalBody,
+  MDBModalFooter,
+  MDBInput,
+} from "mdbreact";
+import { UserContext } from "../../../../providers/UserProvider";
+import { Link } from "react-router-dom";
 import "./RiderProfile.css";
+import male from "../../../../assets/images/avatars/male.jpg";
+import female from "../../../../assets/images/avatars/female.jpg";
+import { firestore } from "../../../firebase";
 
-export class RiderUser extends Component {
-  constructor(props) {
-    super(props);
+const RiderUser = () => {
+  //Top level state context
+  const user = useContext(UserContext);
+  //Ref to current user id for adding wallet data
+  const uid = user.uid;
+  //Toggle state for modal
+  const [modal, setModal] = useState(false);
+  //checks if user has some wallet value in database
+  const [walletExists, setWalletExists] = useState(false);
+  //the actual amount of wallet if funds added or if some value already exists
+  const [walletAmount, setWalletAmount] = useState(null);
+  //unique id for the existing wallet
+  const [existingId, setExistingId] = useState("");
+  //new value of the added wallet funds
+  var newValue = null;
 
-    this.state = {};
-  }
+  useEffect(() => {
+    firestore
+      .collection("weride")
+      .doc("ride_wallet")
+      .collection("wallets")
+      .where("uid", "==", uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          if (doc.exists) {
+            setWalletAmount(doc.data().amount);
+            setWalletExists(true);
+            setExistingId(doc.id);
+          } else {
+            setWalletAmount(0);
+          }
+        });
+      })
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
+      });
+  }, [uid]);
 
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>Profile</title>
-        </Helmet>
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  const updateWalletValue = (e) => {
+    const { value } = e.currentTarget;
+    newValue = value;
+  };
+
+  //function to add funds in database with callbacks
+  const addFunds = () => {
+    var currentValue = walletAmount;
+    var newWalletValue = Number(currentValue) + Number(newValue);
+    setWalletAmount(newWalletValue);
+    console.log(newWalletValue);
+    if (walletExists) {
+      //set a doc
+      setWalletDoc(newWalletValue);
+    } else {
+      //create collection and doc
+      createDoc(newWalletValue);
+    }
+    toggle();
+  };
+
+  //set doc function called in add funds
+  const setWalletDoc = (newWalletValue) => {
+    //set doc
+    firestore
+      .collection("weride")
+      .doc("ride_wallet")
+      .collection("wallets")
+      .doc(existingId)
+      .set(
+        {
+          amount: newWalletValue,
+        },
+        { merge: true }
+      );
+  };
+
+  //create doc function called in cadd funds
+  const createDoc = (newWalletValue) => {
+    //create collection and doc
+    firestore
+      .collection("weride")
+      .doc("ride_wallet")
+      .collection("wallets")
+      .add({
+        amount: newWalletValue,
+        uid: user.uid,
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  };
+
+  return (
+    <div>
+      <Helmet>
+        <title>Profile</title>
+      </Helmet>
+      <div className="mt-5">
         <MDBContainer>
-          <h1>Rider Profile</h1>
+          <MDBRow>
+            <MDBCol md="5">
+              <img
+                src={user.gender === "male" ? male : female}
+                alt="Avatar"
+                className="rider-profile-avatar img-fluid"
+              />
+            </MDBCol>
+
+            <MDBCol md="7" className="text-left">
+              <br />
+
+              <h1>
+                <b>{user.name}</b>
+              </h1>
+              <h4>{user.email}</h4>
+            </MDBCol>
+          </MDBRow>
+          <hr />
+          <MDBRow>
+            <MDBCol md="5">
+              <div>
+                <br />
+                <br />
+                <MDBIcon
+                  icon="wallet"
+                  size="2x"
+                  className="rider-profile-icon"
+                />{" "}
+                {walletAmount ? walletAmount + " PKR" : "0 PKR"}
+              </div>
+              <MDBBtn
+                size="sm"
+                className="rider-profile-btn"
+                color="#00c853"
+                onClick={toggle}
+              >
+                <MDBIcon icon="credit-card" size="1x" /> Add Funds
+              </MDBBtn>
+              <br />
+              <br />
+              <div>
+                <strong className="rider-profile-icon">Package: </strong> Weekly
+                <br />
+                <Link to="rider-packages">
+                  <MDBBtn
+                    size="sm"
+                    className="rider-profile-btn"
+                    color="#00c853"
+                  >
+                    Subscribe to a package
+                  </MDBBtn>
+                </Link>
+              </div>
+            </MDBCol>
+            <MDBCol md="7" className="text-left">
+              <div className="mt-5">
+                <MDBTable borderless responsive>
+                  <MDBTableBody>
+                    <tr>
+                      <td>
+                        <b>Profile Active: </b>
+                      </td>
+                      <td>{user.active ? "Yes" : "No"}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Age:</b>
+                      </td>
+                      <td>{user.age}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Email Address:</b>
+                      </td>
+                      <td>{user.email}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Gender:</b>
+                      </td>
+                      <td>{user.gender === "male" ? "Male" : "Female"}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Name:</b>
+                      </td>
+                      <td>{user.name}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Number:</b>
+                      </td>
+                      <td>{user.number}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Account Type:</b>
+                      </td>
+                      <td>{user.type === "rider" ? "Rider" : "Driver"}</td>
+                    </tr>
+                  </MDBTableBody>
+                </MDBTable>
+              </div>
+            </MDBCol>
+          </MDBRow>
+          {/* Add funds modal */}
+          <MDBContainer>
+            <MDBModal isOpen={modal} toggle={toggle}>
+              <MDBModalHeader toggle={toggle} className="navbar-modal-header">
+                <MDBIcon icon="money-bill-alt" size="2x" /> Add Funds
+              </MDBModalHeader>
+              <MDBModalBody>
+                <MDBRow className="justify-content-center">
+                  <MDBCol md="6">
+                    <MDBInput
+                      label="Amount"
+                      type="number"
+                      onChange={(event) => updateWalletValue(event)}
+                    />
+                  </MDBCol>
+                </MDBRow>
+              </MDBModalBody>
+              <MDBModalFooter className="justify-content-center">
+                <MDBBtn color="success-color-dark" onClick={addFunds}>
+                  Add
+                </MDBBtn>
+              </MDBModalFooter>
+            </MDBModal>
+          </MDBContainer>
         </MDBContainer>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default RiderUser;
