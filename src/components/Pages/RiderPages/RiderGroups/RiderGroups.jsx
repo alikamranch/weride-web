75b36fe3420add9d1aa877b57cef2bf0fe796651
Ali Kamran <43109679+alikamranch@users.noreply.github.com>
2020-05-14 19:53:14 +0500

    Displaying created groups also completed

--- src/components/Pages/RiderPages/RiderGroups/RiderGroups.jsx
+++ src/components/Pages/RiderPages/RiderGroups/RiderGroups.jsx
@@ -1,27 +1,117 @@
-import React, { Component } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Helmet } from "react-helmet";
-import { MDBContainer } from "mdbreact";
+import {
+  MDBContainer,
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCol,
+  MDBRow,
+  MDBAlert,
+  MDBCardImage,
+  MDBCardBody,
+  MDBBtn,
+} from "mdbreact";
 import "./RiderGroups.css";
+import groupsImg from "../../../../assets/images/groups.png";
+import { firestore } from "../../../firebase";
+import { UserContext } from "../../../../providers/UserProvider";
+const RiderGroups = () => {
+  const [hasGroup, setHasGroup] = useState(false);
+  const groupsArray = [];
+  const [groups, setGroups] = useState([]);
+  const [newGroup, setNewGroup] = useState({
+    admin: {},
+    groupMembers: [],
+    name: "",
+  });
+  const user = useContext(UserContext);
+  const uid = user.uid;
 
-class RiderGroups extends Component {
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    firestore
+      .collection("weride")
+      .doc("ride_group")
+      .collection("groups")
+      .where("admin.id", "==", uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          if (doc.exists) {
+            console.log(doc.id, " => ", doc.data());
+            groupsArray.push(doc.data());
+            console.log(groupsArray);
+          }
+        });
+        if (groupsArray.length != 0) {
+          setGroups(groupsArray);
+        }
+      })
+      .then(
+        firestore
+          .collection("weride")
+          .doc("ride_group")
+          .collection("groups")
+          .where("groupMembers", "array-contains", uid)
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              if (doc.exists) {
+                console.log(doc.id, " => ", doc.data());
+                groupsArray.push(doc.data());
+                console.log(groupsArray);
+              }
+            });
+            if (groupsArray.length != 0) {
+              setGroups(groupsArray);
+              console.log(groups);
+            }
+          })
+      )
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
+      });
+  }, [uid]);
 
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>Groups</title>
-        </Helmet>
-        <MDBContainer>
-          <h1>Groups</h1>
+  return (
+    <div>
+      <Helmet>
+        <title>Groups</title>
+      </Helmet>
+      <br />
+      <MDBContainer>
+        <MDBContainer className="text-left">
+          <MDBAlert color="warning" dismiss>
+            <b>Note</b>
+            <br />
+            You can only make a group upto a maximum of 3 people. The person who
+            creates the group would be the admin and will be able to book rides
+            as a group.
+          </MDBAlert>
         </MDBContainer>
-      </div>
-    );
-  }
-}
+        <MDBRow>
+          {groups.map((group, index) => (
+            <MDBCol md="3" className="rider-groups-card" key={index}>
+              <MDBCard>
+                <MDBCardImage className="img-fluid" src={groupsImg} waves />
+                <MDBCardBody>
+                  <MDBCardTitle>{group.name}</MDBCardTitle>
+                  <MDBCardText>
+                    <div>
+                      Admin: {group.admin.name} <br />
+                      {group.groupMembers[0]}
+                      <br />
+                      {group.groupMembers[1]}
+                    </div>
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          ))}
+        </MDBRow>
+      </MDBContainer>
+    </div>
+  );
+};
 
 export default RiderGroups;
--- src/components/Pages/RiderPages/RiderHistory/RiderHistory.jsx
+++ src/components/Pages/RiderPages/RiderHistory/RiderHistory.jsx
@@ -1,27 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
-import { MDBContainer } from "mdbreact";
+import { MDBContainer, MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
 import "./RiderHistory.css";
 
-export class RiderHistory extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>Ride History</title>
-        </Helmet>
-        <MDBContainer>
-          <h1>Ride History</h1>
-        </MDBContainer>
-      </div>
-    );
-  }
-}
+const RiderHistory = () => {
+  let number = 1;
+  return (
+    <div>
+      <Helmet>
+        <title>Ride History</title>
+      </Helmet>
+      <MDBContainer>
+        <div className="mt-5">
+          <MDBTable responsive>
+            <MDBTableHead color="success-color" textWhite>
+              <tr>
+                <th>#</th>
+                <th>Origin</th>
+                <th>Destination</th>
+                <th>Distance</th>
+                <th>Driver</th>
+                <th>Vehicle</th>
+                <th>Total Riders</th>
+                <th>Group</th>
+                <th>Date</th>
+              </tr>
+            </MDBTableHead>
+            <MDBTableBody>
+              <tr>
+                <td>{number}</td>
+                <td>Wapda Town</td>
+                <td>Comsats University Islamabad, Lahore Campus</td>
+                <td>11 KM</td>
+                <td>Ahmad Ali</td>
+                <td>Suzuki Cultus</td>
+                <td>2</td>
+                <td>None</td>
+                <td>13th April, 2020</td>
+              </tr>
+              <tr>
+                <td> {(number += 1)} </td>
+                <td>Wapda Town</td>
+                <td>69 E, Model Town, Lahore</td>
+                <td>8 KM</td>
+                <td>Usman Ali</td>
+                <td>Suzuki WagonR</td>
+                <td>3</td>
+                <td>None</td>
+                <td>18th April, 2020</td>
+              </tr>
+              <tr>
+                <td> {(number += 1)} </td>
+                <td>Wapda Town</td>
+                <td>Governor House, Mall Road, Lahore</td>
+                <td>22 KM</td>
+                <td>Ahmed Salim</td>
+                <td>Suzuki WagonR</td>
+                <td>2</td>
+                <td>None</td>
+                <td>1st May, 2020</td>
+              </tr>
+            </MDBTableBody>
+          </MDBTable>
+        </div>
+      </MDBContainer>
+    </div>
+  );
+};
 
 export default RiderHistory;
